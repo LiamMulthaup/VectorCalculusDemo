c3486c67671b37f6a62b202429bf45dbadd55d4a
Liam Multhaup <LiamMulthaup@users.noreply.github.com>
2019-04-07 20:19:51 -0700

    Initial commit

---
+++ Workspace/VectorCalculusDemo/src/Objects3D/Control3D.java
@@ -0,0 +1,17 @@
+package Objects3D;
+
+import java.awt.Graphics;
+
+public class Control3D 
+{
+	Point3D location;
+	Panel3D panel;
+	public Control3D(Point3D location)
+	{
+		this.location = new Point3D(location);
+	}
+	public void paint(Graphics g)
+	{
+		
+	}
+}
